fb6d690d1c695f93d37b5a88f7838b0128824efd
Dung <21119301@student.hcmute.edu.vn>
2023-01-09 15:48:06 +0700

    set up fileName for all needed sections

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 1000;
+const path = require('path');
 const morgan = require('morgan')
+//Add template Pug
+const pug = require('pug');
+app.set('views', path.join(__dirname, 'resources/views'))
+app.set('view engine', 'pug')
 app.use(morgan('combined'))
 
 
 
-
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home')
 })
 
 app.listen(port, () => {
